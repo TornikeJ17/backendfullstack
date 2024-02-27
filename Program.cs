3d4db10d5a3e510483eca5b3206fdc85f4cc5d66
TornikeJ17 <jmukhadze.tornike@hotmail.com>
2024-02-27 18:22:00 +0400

    fixed

--- Program.cs
+++ Program.cs
@@ -46,17 +46,14 @@ builder.Services.AddCors(options =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-//builder.Services.AddDbContext<DataContext>(options =>
-//{
-    // This line of code retrieves the connection string named "DefaultConnection"
-    // from your application's configuration, which is usually set in appsettings.json.
-    // It then configures the DataContext to use SQL Server with that connection string.
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//});
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
 
 builder.Services.AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<DataContext>();
